e315e8ebe8530eb9f24d4db4987fc3aa0e209455
LenKIM <joenggyu0@gmail.com>
2021-10-10 15:46:59 +0900

    add RUN&TERMINATE container

--- shell/run_container.sh
+++ shell/run_container.sh
@@ -1,7 +1,7 @@
 v=$(git rev-parse --short HEAD)
 
-echo "$CI_FLAG"
+echo "Start Run Container"
 
-#docker run --rm --name local-postgre -d \
-#       -p 5430:5432 -v "${PWD}"/schema:/docker-entrypoint-initdb.d \
-#       joenggyu0/postgre:0.0.1
+docker run --rm --name local-postgre -d \
+       -p 5430:5432 -v "${PWD}"/schema:/docker-entrypoint-initdb.d \
+       joenggyu0/postgre:0.0.1
